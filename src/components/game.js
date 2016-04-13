178edc56593b81d0738940de6f9a036100ed1abf
Devin Otway <devin.otway@gmail.com>
2016-04-13 00:28:48 -0700

    move board initialization into board class

--- src/components/game.js
+++ src/components/game.js
@@ -10,7 +10,7 @@ export class Game extends React.Component {
 			minesLeft: 10,
 			timer: 0,
 			// graph of interconnected cells
-			board: null,
+			board: new Board(),
 			// map of rowcol ('00' for 0th row, 0th col) to nodes
 			index: {}
 		}
@@ -27,57 +27,12 @@ export class Game extends React.Component {
 		, 1000)
 	}
 
-	createGame() {
-		let indices = createIndices()
-		let mineLocations = chooseBombIndices()
-		mineLocations.forEach((location)=>{
-			indices[location] = new Tile('mine')
-		})
-
-		for (let [location, node] of indices) {
-			if (!node) {
-				indices[location] = new Tile('blank')
-			}
-		}
-	}
+	// updateBoardState({}){
+	// 	this.setState(Object.assign({}, this.state.board, newState))
+	// }
 
 	createBoard() {
 		
-	}
-
-	createIndices(size) {
-		let indices = {}
-		let row, col = 0
-
-		for (; i < size; i++) {
-			for (; j < size; j++) {
-				indices['' + i + j] = void 0
-			}
-		}
-
-		return indices
-	}
-
-	chooseMineIndices(size) {
-		let count = 0
-		let indices = {}
-		let indicesArray = []
-		while (count < size) {
-			chooseIndex()
-			count++
-		}
-
-		function chooseIndex() {
-			let row = Math.floor(Math.random(size))
-			let col = Math.floor(Math.random(size))
-			if (!indices['' + row + col]) {
-				indicesArray.push('' + row + col)
-			} else {
-				chooseIndex()
-			}
-		}
-
-		return indicesArray
 	}
 
 	resetTimer() {
--- src/utils/Board.js
+++ src/utils/Board.js
@@ -12,6 +12,8 @@ export class Tile {
 		this.row = null
 		this.col = null
 		this.isVisible = false
+		// initialized to value of 0 and set when game state is initialized
+		this.adjacentMines = 0	
 
 		switch(cellType) {
 			case cellTypes.MINE:
@@ -20,8 +22,6 @@ export class Tile {
 
 			case cellTypes.BLANK:
 				this.type = cellTypes.BLANK
-				// initialized to value of 0 and set when game state is initialized
-				this.adjacentMines = 0
 				break
 		}
 	}
@@ -54,8 +54,10 @@ export class Tile {
 }
 
  export class Board {
-	constructor(){
+	constructor(size){
 		this.nodes = [];
+		this.index = {}
+		this.size = size
 	}
 
 	find(id) {
@@ -72,12 +74,123 @@ export class Tile {
 		}
 	}
 
+	addNode(node) {
+		this.nodes.push(node)
+	}
+
+	copy() {
+		return JSON.parse(JSON.stringify(this))
+	}
+
+	createGame() {
+		this.index = createIndices()
+		let mineLocations = chooseMineIndices()
+
+		// add mines to randomly assigned locations
+		mineLocations.forEach((location)=>{
+			mine = new Tile('mine')
+			this.index[location] = mine
+			this.addNode(mine)
+		})
+
+		// fill in gaps with blank tiles (no mines)
+		for (let [location, node] of this.index) {
+			if (!node) {
+				this.index[location] = new Tile('blank')
+			}
+		}
+
+		// connect all nodes to their adjacent nodes
+		for (let [location, node] of this.index) {
+			let row = parseInt(location.substr(0,1))
+			let col = parseInt(location.substr(1,2))
+			let adjacentNodes = getAdjacentNodes(row, col)
+			adjacentNodes.forEach((adjacentNode)=>{
+				this.addEdge(node, adjacentNode)
+			})
+		}
+	}
+
+	getAdjacentNodes(row, col) {
+		let r0 = row
+		let r1 = row - 1 
+		let r2 = row + 1
+		let rows = [r0, r1, r2]
+
+		let c0 = col
+		let c1 = col - 1
+		let c2 = col + 1
+		let cols = [c0, c1, c2]
+
+		return generateCoordList()
+
+		function generateCoordList() {
+
+			let coordList = []
+
+			let rowsNew = _.reject(rows, (row)=>{
+				return row < 0 || row > this.size - 1
+			})
+
+			let colsNew = _.reject(cols, (col)=>{
+				return col < 0 || row > this.size - 1
+			})
+
+			rowsNew.forEach((row)=>{
+				colsNew.forEach((col)=>{
+					coordList.push([row, col])
+				})
+			})
+
+			return coordList.map((coords)=>{
+				return coords.join('')
+			})
+		}
+	}
+
 	printNodes() {
 		let i = 0, l = this.nodes.length
 		for (; i < l; i++) {
 			console.log(this.nodes[i].id + ':')
 			console.log(this.nodes[i].edges)
 		}
+	}
+
+	chooseMineIndices() {
+		let size = this.size
+		let count = 0
+		let indices = {}
+		let indicesArray = []
+		while (count < size) {
+			chooseIndex()
+			count++
+		}
+
+		function chooseIndex() {
+			let row = Math.floor(Math.random() * size)
+			let col = Math.floor(Math.random() * size)
+			if (!indices['' + row + col]) {
+				indicesArray.push('' + row + col)
+			} else {
+				chooseIndex()
+			}
+		}
+
+		return indicesArray
+	}
+
+	createIndices() {
+		let size = this.size
+		let indices = {}
+		let row, col = 0
+
+		for (; i < size; i++) {
+			for (; j < size; j++) {
+				indices['' + i + j] = void 0
+			}
+		}
+
+		return indices
 	}
 }
 
