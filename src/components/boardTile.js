83b7d3f74cdd3df6bb1ff219034218c88ac8b95c
Devin Otway <devin.otway@gmail.com>
2016-05-16 23:59:00 -0700

    refactor class conditionality

--- src/components/boardTile.js
+++ src/components/boardTile.js
@@ -4,39 +4,57 @@ import classNames from 'classnames';
 export class BoardTile extends React.Component {
 	constructor(props) {
 		super(props)
+		this.state = {
+			rightClicked: false
+		}
 	}
 
 	checkTile() {
 		this.props.checkTile(this.props.location)
 	}
 
+	rightClick(event) {
+		event.preventDefault()
+		this.setState({
+			rightClicked: !this.state.rightClicked
+		})
+	}
+
+	handleClick(event) {
+		// better to unbind and then rebind, this will cause a memory leak
+		// if the board is disabled after game over or a flag is currently applied
+		if (this.props.disableBoard || this.state.rightClicked) {	
+			return;
+		} else {
+			this.checkTile()
+		}
+	}
+
 	render() {
 		let tile = this.props.index[this.props.location]
-		let prefix = 'mc-'
-		let clazz = ''
-		let adjacentMines = ''
-		let clickHandler = void 0
+		
 
-		// better to unbind and then rebind, this will cause a memory leak
-		if (this.props.disableBoard) {
-			clickHandler = () => {
-				return void 0
-			}
-		} else {
-			clickHandler = this.checkTile.bind(this)
+		let covered = !tile.checked && !tile.wasClicked && !this.state.rightClicked
+		let numAdjacentClass = ''
+		if (tile.checked && tile.type !== 'mine') {
+			numAdjacentClass = `mc-${tile.adjacentMines}`
 		}
+		
+		let tileClass = classNames({
+			'mc-covered': covered,
+			'mc-mine em em-alien': tile.wasClicked && tile.type === 'mine',
+			'mc-flagged': !tile.checked && this.state.rightClicked
+		}, numAdjacentClass)
 
-		if (!tile.checked && !tile.wasClicked) {
-			clazz = prefix + 'covered'
-		} else {
-			if (tile.type === 'mine') {
-				clazz = 'mc-mine em em-alien'
-			} else {
-				clazz = prefix + tile.adjacentMines
-				adjacentMines = tile.adjacentMines
+		let innerText = ''
+		if (!covered && !this.state.rightClicked) {
+			if (tile.type !== 'mine') {
+				if (tile.adjacentMines > 0) {
+					innerText = tile.adjacentMines;		
+				}
 			}
 		}
 
-		return <td onClick={clickHandler} className={clazz}>{adjacentMines}</td>
+		return <td onClick={this.handleClick.bind(this)} onContextMenu={this.rightClick.bind(this)} className={tileClass}>{innerText}</td>
 	}
 }
